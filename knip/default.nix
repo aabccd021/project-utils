eac4bb53a54a872f1f08b9e90ffb357d33d0f8f0
aabccd021 <aabccd021@gmail.com>
2024-12-11 13:10:53 +0700

    Refactor: update installation process for knip package

--- knip/default.nix
+++ knip/default.nix
@@ -3,7 +3,14 @@ let
   nodeModules = buildNodeModules.fromLockJson ./package.json ./package-lock.json;
 
 in
-pkgs.writeShellScriptBin "knip" ''
-  ${pkgs.bun}/bin/bun run ${nodeModules}/knip/bin/knip.js "$@"
+# pkgs.writeShellScriptBin "knip" ''
+#   ${pkgs.bun}/bin/bun run ${nodeModules}/knip/bin/knip.js "$@"
+# ''
+
+pkgs.runCommandLocal "knip" { } ''
+  mkdir -p "$out/bin"
+  cp -Lr ${nodeModules} $out/node_modules
+  echo "${pkgs.bun}/bin/bun run $out/node_modules/knip/bin/knip.js" > $out/bin/knip
+  chmod +x $out/bin/knip
 ''
 
