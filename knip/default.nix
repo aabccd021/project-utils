a8dfdcd83bbc13ccacca7670187c257613805f48
aabccd021 <aabccd021@gmail.com>
2024-12-11 13:11:28 +0700

    Remove obsolete shell script for knip command.

--- knip/default.nix
+++ knip/default.nix
@@ -1,12 +1,7 @@
 { pkgs, buildNodeModules }:
 let
   nodeModules = buildNodeModules.fromLockJson ./package.json ./package-lock.json;
-
 in
-# pkgs.writeShellScriptBin "knip" ''
-#   ${pkgs.bun}/bin/bun run ${nodeModules}/knip/bin/knip.js "$@"
-# ''
-
 pkgs.runCommandLocal "knip" { } ''
   mkdir -p "$out/bin"
   cp -Lr ${nodeModules} $out/node_modules
