02f27828179257cc47d50bf2ae911be2d2754d8a
aabccd021 <aabccd021@gmail.com>
2025-01-19 10:33:23 +0700

    Fix formatting and conditionals in checkpoint.sh and comment out lines in flake.nix.

--- checkpoint.sh
+++ checkpoint.sh
@@ -1,8 +1,8 @@
 git add -A >/dev/null
 
 has_formatter=$(nix flake show --json | nix run nixpkgs#jq -- '.formatter["x86_64-linux"]' || true)
-if [ -n "$has_formatter" ]; then
-  echo "Has formatter: $has_formatter"
+echo "Has formatter: $has_formatter"
+if [ -n "$has_formatter" ] && [ "$has_formatter" != "null" ]; then
   start=$(date +%s)
   nix fmt
   echo "'nix fmt' finished successfully in $(($(date +%s) - start))s"
@@ -11,8 +11,8 @@ fi
 git add -A >/dev/null
 
 has_checks=$(nix flake show --json | nix run nixpkgs#jq -- '.checks["x86_64-linux"]' || true)
+echo "Has checks: $has_checks"
 if [ -n "$has_checks" ]; then
-  echo "Has checks: $has_checks"
   start=$(date +%s)
   nix flake check --log-lines 200 --quiet || (git reset >/dev/null && exit 1)
   echo "'nix flake check' finished successfully in $(($(date +%s) - start))s"
