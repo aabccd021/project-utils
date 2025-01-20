e38727276d0acec398d4a0c3f0d54ffdcc0b3799
aabccd021 <aabccd021@gmail.com>
2025-01-20 10:59:07 +0700

    chore: Update gcroot-all and add checkpoint-fix script for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -99,6 +99,19 @@
 
       formatter.x86_64-linux = treefmtEval.config.build.wrapper;
 
+      apps.x86_64-linux = {
+        checkpoint-fix = {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "checkpoint-fix" ''
+              echo "running checkpoint-fix"
+              sleep 1
+              echo "done checkpoint-fix"
+            ''
+          );
+        };
+      };
+
       lib = {
 
         inherit buildNodeModules;
